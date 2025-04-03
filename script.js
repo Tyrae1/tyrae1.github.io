37818ede5f609ab96f521e3a0bf379d6e5d9c7a3
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-04-03 19:44:43 +0300

    deleted js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", function() {
-    document.body.classList.remove("preload");
-    document.querySelector(".preloader").classList.add("hidden");
-
-    // Опціонально: Видалити елемент повністю після зникнення
-    setTimeout(() => {
-        document.querySelector(".preloader").remove();
-    }, 500);
-});
