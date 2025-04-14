617fdeb82981876c50255c74310d3378d15ba015
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-04-14 20:43:58 +0300

    sync

---
+++ script.js
@@ -0,0 +1,7 @@
+//Типи даних - примітивні і комплексні. В різних огортках.
+// Примітивні типи: string - 'string', "string"
+console.log("hello world");
+console.log(5);
+console.log("hello world" + 5);
+console.log(5 + 5);
+console.log(`false`);
