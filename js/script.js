4d28c0b6ef596399b74e7ac512933edcebb587d3
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-05-28 21:45:36 +0300

    Ending on homework 7

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
-const userName = `Ivan`;
-let userAge = 28;
-let userVerification = true;
-const userRegistrationDate = new Date(2021, 10, 28);
-const userCardNumber = 156484486474684848n;
-let userPromoCode = null;
-let userReserveName;
-let userBonuses = 235;
+'use strict';
+const userName = prompt(`Hello! What is your name?`);
+// Спочатку змінні не вдавалося вивести в умові - вивів їх окремо на початку умови.
+let userAge;
+let userBonusCode;
+let userSecondName;
+let userConfirm;
+let userId;
+if (userName){
+    const inputUserAge = prompt(`What is your age, ${userName}?`);
+    userAge = Number(inputUserAge);
+    if (isNaN(userAge)){
+        alert(`Please, ${userName}, enter a valid age`);
+    } else {
+        userConfirm = confirm(`${userName} would you like to confirm that you ${userAge} years old and continue?`);
+        if (userConfirm) {
+            userId= 534165415646454n;
+            userBonusCode = null;
+            alert(`
+        Hello, ${userName}!
+        Your age: ${userAge}
+        Your status: Participation confirmed
+        `)
+        } else {
+            alert(`
+        Hello, ${userName}!
+        Your age: ${userAge}
+        Your status: Participation refused
+        `);
+        }
+    }
+} else {
+    alert("Goodbye!");
+}
+// Перевірка змінних
+console.log(`User name:`, userName, typeof userName);
+console.log(`User age:`, userAge, typeof userAge);
+console.log(`User confirm:`, userConfirm, typeof userConfirm);
+console.log(`User ID:`, userId, typeof userId);
+console.log(`User bonus code:`, userBonusCode, typeof userBonusCode);
+console.log(`User second name:`, userSecondName, typeof userSecondName);
