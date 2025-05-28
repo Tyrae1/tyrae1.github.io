03701aa0cdd304b1bdbd44b7f21d562cd8452084
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-05-28 20:37:16 +0300

    adding files

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const userName = `Ivan`;
+let userAge = 28;
+let userVerification = true;
+const userRegistrationDate = new Date(2021, 10, 28);
+const userCardNumber = 156484486474684848n;
+let userPromoCode = null;
+let userReserveName;
+let userBonuses = 235;
