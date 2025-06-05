0714b1d1b93bf9856509f16be7d5c49c255ccc45
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-06-05 20:50:02 +0300

    Finishing howe work 14

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let num1 = +prompt("Write a first number!");
+let num2 = +prompt("Write a second number!");
+let num3 = +prompt("Write a third number!");
+if (!isNaN(num1) && !isNaN(num2) && !isNaN(num1)) {
+    let sum = num1 + num2 + num3;
+    alert("Average number is: " + sum/3);
+} else {
+    alert("Please enter a number");
+}
