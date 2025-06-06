76455e6ca8f3fbda7c6786ef5915d06ade160cd3
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-06-06 18:55:41 +0300

    Finishing hw-15

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict';
-let num1 = +prompt("Write a first number!");
-let num2 = +prompt("Write a second number!");
-let num3 = +prompt("Write a third number!");
+const num1 = +prompt("Write a first number!");
+const num2 = +prompt("Write a second number!");
+const num3 = +prompt("Write a third number!");
 if (!isNaN(num1) && !isNaN(num2) && !isNaN(num1)) {
-    let sum = num1 + num2 + num3;
+    const sum = num1 + num2 + num3;
     alert("Average number is: " + sum/3);
 } else {
     alert("Please enter a number");
