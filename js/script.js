aa16af07e15053517a1dd4ec56a61d36f1f1a261
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-06-06 18:57:08 +0300

    Finishing hw-13

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 'use strict';
-let timeHours = +prompt('How many hours you have?');
-let timeSeconds = timeHours * 3600;
+const timeHours = +prompt('How many hours you have?');
+const timeSeconds = timeHours * 3600;
 alert(`You have ${timeSeconds} seconds in it.`);
