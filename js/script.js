28511a909fc23017d471bed1ce6143b680d25d9b
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-06-10 20:56:50 +0300

    finishing on HW17

--- js/script.js
+++ js/script.js
@@ -11,43 +11,51 @@ const dollar = 27;
 for (let i = 10; i <= 100; i+=10) {
     resultSecond += `${i}$ is ${i*dollar} UAH.\n`;
 }
-alert(`Curency calculation is:\n${resultSecond}`);
+alert(`Currency calculation is:\n${resultSecond}`);
 
 
 let resultThird = '';
 let resultFour = true;
-let resultFive = true;
+let resultFive = false;
 let testNumber = Number(prompt('Enter integer number!'));
-for (let i = 1; i <= 100; i++) {
-    if ((i**2) <= testNumber && i % 1 === 0) {
-        resultThird += i + ` `;
-    }
+let resultFivePower = 1;
+let power = 0;
+if (isNaN(testNumber)) {
+    alert(`${testNumber} - It's not a number!`);
+} else {
+    for (let i = 1; i <= 100; i++) {
+        if ((i**2) <= testNumber && Number.isInteger(i)) {
+            resultThird += i + ` `;
+        }}
     if (testNumber <= 1) {
-        resultFour = false;
+            resultFour = false;
     } else {
-        for (let i = 2; i < testNumber; i++) {
+        for (let i = 2; i <= Math.sqrt(testNumber); i++) {
             if (testNumber % i === 0) {
                 resultFour = false;
-                break;
             }
-        }
+        } }
+        if (testNumber < 1) {
+            resultFive = false;
+            alert(`${testNumber} - must be more then 0!`);
+        } else {
+            for (; resultFivePower <= testNumber ; power++) {
+                if (resultFivePower === testNumber) {
+                    resultFive = true;
+                    break;
+                }
+                resultFivePower = resultFivePower *3;
+            }
     }
-    if (testNumber < 1) {
-        resultFive = false;
-    } else {
-
-    }
-
 }
 alert(`Even numbers from 1 to 100 whose square doesn't exceed your number are: ${resultThird}`);
 if (resultFour) {
-    alert(`${testNumber} is prime number`);
+    alert(`${testNumber} is prime number!`);
 } else {
-    alert(`${testNumber} isn't prime number`);
+    alert(`${testNumber} isn't prime number!`);
 }
-    alert(``
 if (resultFive) {
-    alert(`You can get ${testNumber} by raising 3 to the power of ${testNumberPower}!`);
+    alert(`${testNumber} can be found by powering 3 in ${power}!`);
 } else {
-    alert(`You can't get ${testNumber} by raising 3 to the power!`);
+    alert(`${testNumber} can't be found by powering 3!`);
 }
