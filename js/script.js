ca2ac9d0a4ab4efc1bbd511385f92d62f6e73b4b
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-06-16 20:29:00 +0300

    Finishing HW-20.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const ourLength = Number(prompt('Enter character length'));
+let generateKey = (length, characters) => {
+    let result = '';
+    for (let i=0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+const key = generateKey(ourLength, characters);
+console.log(key);
+alert(`Your generated key is: ${key}`);
