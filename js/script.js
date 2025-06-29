e303b09ab4d9b6680abd6996161064dad25e03e1
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-06-29 11:33:57 +0300

    Finishing hw-32

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 'use strict';
+const data = {
+    id: 1,
+    name: "root",
+    meta: {
+        id: 2,
+        parent: {
+            id: 3,
+            name: "leaf",
+        },
+    },
+    array: [
+        { id: 4 },
+        { name: "node", children: [{ id: 5 }] },
+    ],
+};
+function findValuesByKey(obj, targetKey) {
+    let result = [];
+
+        for (let key in obj){
+            const value = obj[key];
+            if (key === targetKey) {
+                result.push(value);
+            }
+    if (typeof value === 'object' && value !== null) {
+            const nestedResult = findValuesByKey(value, targetKey);
+            result = result.concat(nestedResult);
+    }
+    }
+    return result;
+}
+findValuesByKey(data, "id");
+findValuesByKey(data, "name");
+console.log(findValuesByKey(data, "id"));
+console.log(findValuesByKey(data, "name"));
+// ➜ [1, 2, 3, 4, 5]
