8729de50e4c6ee5bbd875591a9902ccaec2ba094
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-07-02 19:40:37 +0300

    Finishing hw-33

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+const button = document.getElementById("toggle-theme");
+button.addEventListener("click", () => {
+    button.classList.toggle("active");
+    if (button.classList.contains("active")) {
+        document.body.style.backgroundColor = "#413C58";
+    } else {
+        document.body.style.backgroundColor = "";
+    }
+});
