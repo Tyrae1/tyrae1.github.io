b19730f9276445b5ca40ee3751177be271eb3d13
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-07-03 20:10:54 +0300

    Finishing hw-34

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+document.getElementById("filter").addEventListener("change", (e) => {
+    const selectedValue = e.target.value;
+    const cards = document.getElementsByClassName("card");
+    for (let i = 0; i < cards.length; i++) {
+        let card = cards[i];
+        if (selectedValue === "all") {
+            card.style.display = "block";
+        } else {
+            if (card.dataset.category === selectedValue) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        }
+    }
+});
+
