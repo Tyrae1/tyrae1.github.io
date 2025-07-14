bdb9fe76386f32fa559bdc29e9d6a01fa6de1dfc
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-07-14 20:56:31 +0300

    Working on controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,19 @@
 'use strict';
+//DOM initialization:
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('[data-form]');
+    const submitBtn = form.querySelector('[type="submit"]');
+    formValidation(form, submitBtn);
+        form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const product = createUserInterface(form);
+        if (isNaN(product.prodPrice) || Number(product.prodPrice) <= 0) {
+        showErrors(['Price lower then 0!']);
+        return;
+        }
+        product.id = Date.now().toString();
+        addProduct(product);
+        clearForm(form);
+        window.location.href = 'list.html';
+        });
+});
