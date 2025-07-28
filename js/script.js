2bda6d02baa73a642e475cd1796489a00436ecd0
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-07-28 21:26:02 +0300

    Reworked with student

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 function fillAttendanceRandom(student, presenceProbability = 0.7) {
-    for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < student.lessonsCount; i++) {
         if (Math.random() < presenceProbability) {
             student.present();
         } else {
@@ -16,9 +16,15 @@ fillAttendanceRandom(student1, 0.9);
 fillAttendanceRandom(student2, 0.7);
 fillAttendanceRandom(student3, 0.5);
 
-student1.studRating.push(95, 99, 85, 90, 84);
-student2.studRating.push(75, 70, 90, 80, 97);
-student3.studRating.push(66, 68, 68, 70, 65);
+student1.setMark(95);
+student2.setMark(75);
+student3.setMark(66);
+student1.setMark(90);
+student2.setMark(70);
+student3.setMark(60);
+student1.setMark(95);
+student2.setMark(80);
+student3.setMark(75);
 
 console.log(student1.getFullName(), student1.getAge(), student1.getAverageMark(), student1.summary());
 console.log(student2.getFullName(), student2.getAge(), student2.getAverageMark(), student2.summary());
--- js/student.js
+++ js/student.js
@@ -1,44 +1,84 @@
 'use strict';
 class Student {
-constructor (firstName, lastName, birthYear) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthYear = birthYear;
-    this.studRating = [];
-    this.attendance = new Array(25).fill(null);
-}
-getFullName() {
-    return this.firstName + " " + this.lastName;
-}
-getAge() {
-    return (new Date().getFullYear() - this.birthYear);
-}
-getAverageMark() {
-    if (this.studRating.length === 0) {
-        return 0;
-    } else {
-        return (this.studRating.reduce((acc, curr) => acc + curr, 0) / this.studRating.length);
+    constructor (firstName, lastName, birthYear, lessonsCount = 25) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.lessonsCount = lessonsCount;
+        this.marks = new Array(lessonsCount);
+        this.attendances = new Array(lessonsCount);
+        this.currentLesson = 1;
+        this.minMark = 0;
+        this.maxMark = 100;
     }
-}
-present() {
-    if (this.attendance.length < 25) {
-        this.attendance.push(true);
+    getFullName() {
+        return this.firstName + " " + this.lastName;
     }
-}
-absent() {
-    if (this.attendance.length < 25) {
-        this.attendance.push(false);
+    getAge() {
+        return new Date().getFullYear() - this.birthYear;
     }
-}
-summary() {
-    const attendanceRate = this.attendance.filter(Boolean).length / this.attendance.length;
-    const averageMark = this.getAverageMark();
-    if (attendanceRate > 0.9 && averageMark > 90) {
-       return  `Молодець!`;
-    } else if (attendanceRate > 0.9 || averageMark > 90) {
-        return `Добре але можна краще!`;
-    } else {
-        return `Редиска!`;
+    _getCurrentLessonIndex () {
+        return this.currentLesson - 1;
     }
-}
+    getAverageMark() {
+        if (this.marks.length === 0) {
+            return 0;
+        } else {
+            return (this.marks.reduce((acc, curr) => acc + curr, 0) / this.marks.length);
+        }
+    }
+    _setAttendance (isPresent) {
+        if (typeof isPresent !== 'boolean') {
+            throw new Error('isPresent should be a boolean');
+        }
+        if (this.currentLesson > this.lessonsCount) {
+            throw new Error(`lessonsCount must be less than or equal ${this.lessonsCount}`);
+        }
+        this.attendances[this._getCurrentLessonIndex()] = isPresent;
+        this.currentLesson += 1;
+    }
+    _calcAvgMark() {
+        const marksData = this.marks.reduce((acc, mark) => {
+             acc.sum += mark;
+             acc.count += 1;
+            return acc;
+        }, {sum: 0, count: 0});
+        return Math.floor(marksData.sum / marksData.count);
+    }
+    _calcAvgAttd() {
+        const { visited, count} = this.attendances.reduce((acc, attd) => {
+            if (attd) acc.visited += 1;
+            if (typeof attd === 'boolean') acc.count += 1;
+            return acc;
+        }, {visited: 0, count: 0});
+        return Number((visited / count).toFixed(2))
+    }
+    present() {
+        this._setAttendance(true);
+    }
+    absent() {
+        this._setAttendance(false);
+    }
+    setMark(mark) {
+        if(typeof mark !== "number" || isNaN(mark) ) throw new Error('mark should be a number');
+        if(mark < this.minMark || mark > this.maxMark) throw new Error('mark must be less than 100 and more than 0');
+
+        const currentLessonIndex = this._getCurrentLessonIndex() - 1;
+        if(this.attendances[currentLessonIndex]) {
+            this.marks[currentLessonIndex] = mark;
+        } else {
+            console.warn('You cannot set mark for this lesson');
+        }
+    }
+    summary() {
+        const attendanceRate = this._calcAvgAttd();
+        const averageMark = this._calcAvgMark();
+        if (attendanceRate > 0.9 && averageMark > 90) {
+            return  `Молодець!`;
+        } else if (attendanceRate > 0.9 || averageMark > 90) {
+            return `Добре але можна краще!`;
+        } else {
+            return `Редиска!`;
+        }
+    }
 }
