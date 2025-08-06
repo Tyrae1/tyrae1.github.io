418ecbc20762925248dc157d8392e22ead1bd019
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-08-06 19:30:57 +0300

    Finished with hw-44

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+class HistoryTracker {
+    constructor() {
+        this.visited = [];
+        window.addEventListener('popstate', event => {
+            console.log('popstate triggered!');
+            console.log('current url:', location.href);
+            console.log('State:', event.state);
+        });
+    }
+    push(url){
+        this.visited.push(url);
+        history.pushState({from: 'tracker'}, '', url);
+    }
+    back(){
+        history.back();
+    }
+}
+const tracker = new HistoryTracker();
+tracker.push('?page=1');
+tracker.back();
+tracker.push('?page=2');
+tracker.back();
