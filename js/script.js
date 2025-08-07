484db4b3b0bebbabf796f89520e3d7f55bd03896
Kostiantyn Gordienko <bulletproofua94@gmail.com>
2025-08-07 10:12:06 +0300

    Finishing hw-46

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+class URLParser {
+    constructor(url){
+        this.url = new URL(url);
+    }
+    get protocol() {
+        return this.url.protocol;
+    }
+    get hostname() {
+        return this.url.hostname;
+    }
+    get path() {
+        return this.url.pathname;
+    }
+    get queryParams() {
+        const params = {};
+        for (const [key, value] of this.url.searchParams.entries()) {
+            params[key] = value;
+        }
+        return params;
+    }
+}
+
+const parser = new URLParser("https://example.com/products/item?search=book&page=2");
+console.log(parser.protocol);
+console.log(parser.hostname);
+console.log(parser.path);
+console.log(parser.queryParams);
