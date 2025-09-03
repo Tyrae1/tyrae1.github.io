8ce404260fee0384eb2356f41e98d8a7cc848e83
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-09-03 20:08:52 +0300

    Finishing hw-52

--- js/script.js
+++ js/script.js
@@ -11,18 +11,18 @@ function fetchUserData(id) {
         }, 1000);
     })
 }
-function getUsersData(userIds) {
-    const promises = userIds.map(userId => fetchUserData(userId));
-    return Promise.allSettled(promises).then(result => {
-        console.log('All settled:',result);
-        const success = result
-            .filter(r => r.status === 'fulfilled')
-            .map(r => r.value);
-        const errors = result
-            .filter(r => r.status === 'rejected')
-            .map(r => r.reason);
+async function getUsersData(userIds) {
+    try {
+        const promises = userIds.map(userId => fetchUserData(userId));
+        const result = await Promise.allSettled(promises);
+        console.log('All settled:', result);
+        const success = result.filter(r => r.status === 'fulfilled').map(r => r.value);
+        const errors = result.filter(r => r.status === 'rejected').map(r => r.reason);
         return {success, errors};
-    });
+    } catch (err) {
+        console.error('Ops something went wrong', err);
+        return {success: [], errors: []};
+    }
 }
 getUsersData(userIds).then((result) => {
     console.log("✅ Success:", result.success);
